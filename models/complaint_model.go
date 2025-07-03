650ecd5facc07587075f8da20c41b93fc9d9b768
Rama Diaz <ramadiaz221@gmail.com>
2025-07-03 13:38:16 +0700

    refactor: add Status into models.Complains

--- models/complaint_model.go
+++ models/complaint_model.go
@@ -6,13 +6,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type ComplaintStatus string
+
+const (
+	Open    ComplaintStatus = "open"
+	Process ComplaintStatus = "process"
+	Closed  ComplaintStatus = "closed"
+)
+
 type Complaints struct {
 	gorm.Model
 
-	ID          int64  `gorm:"primaryKey"`
-	UUID        string `gorm:"not null;unique;index"`
-	Title       string `gorm:"not null"`
-	Description string `gorm:"not null"`
+	ID          int64           `gorm:"primaryKey"`
+	UUID        string          `gorm:"not null;unique;index"`
+	Title       string          `gorm:"not null"`
+	Description string          `gorm:"not null"`
+	Status      ComplaintStatus `gorm:"not null;default:open"`
 
 	CreatedAt time.Time  `gorm:"not null"`
 	UpdatedAt time.Time  `gorm:"not null"`
